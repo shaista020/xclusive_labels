edd1c5b5d675b7f2dcd8407c116dbb2afba2b2c4
junaidanwar080 <junaidanwar080@gmail.com>
2025-01-08 10:48:34 +0500

    debug Fasle and host allowed

--- contact_project/settings.py
+++ contact_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jte%pwlw%nt(b2uag++nh3mf529d^3!s)-!yb3bfe2vspszst4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['143.244.177.2']
+ALLOWED_HOSTS = ['143.244.177.2']
 
 
 # Application definition
