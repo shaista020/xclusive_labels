f932dc71bb89ec32ee83ddc664f36c55ef653d97
junaidanwar080 <junaidanwar080@gmail.com>
2025-01-08 15:13:36 +0500

    media folder added

--- contact_project/settings.py
+++ contact_project/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jte%pwlw%nt(b2uag++nh3mf529d^3!s)-!yb3bfe2vspszst4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['143.244.177.2']
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['143.244.177.2']
 
 
 # Application definition
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'contact_form',
     'adminside',
     'authentication',
-    'debug_toolbar',
+    # 'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -53,12 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 # Debug Toolbar settings (for local development only)
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
+# INTERNAL_IPS = [
+#     '127.0.0.1',
+# ]
 AUTH_USER_MODEL = 'adminside.CustomUser'
 
 ROOT_URLCONF = 'contact_project.urls'
@@ -127,10 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
